b5083d0b25c1294abaf69b7ab273b5589a5f594c
@s Name CChnaged <akkr2017@gmail.com>
2023-03-10 16:55:49 +0530

    Changes Made

--- app/src/main/java/com/example/githubdemo/MainActivity.kt
+++ app/src/main/java/com/example/githubdemo/MainActivity.kt
@@ -34,6 +34,7 @@ class MainActivity : ComponentActivity() {
                     //global changed
                     //commit -a
                     //stahs
+                    //made changes
                 }
             }
         }
